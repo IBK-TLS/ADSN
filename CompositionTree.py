bc47d4960339875cf34d24241e90084f34aaae60
grj <geoffrey.roman.jimenez@gmail.com>
2019-09-29 11:46:11 +0200

    modified:   CompositionTree.py

--- CompositionTree.py
+++ CompositionTree.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import argparse
 import random
+import uuid 
+
 
 # write a cost function (entropy/Gini index ?)
 
@@ -70,114 +72,109 @@ def gini_impurity(data):
     prob  = np.array(prob)
     return np.sum(prob*(1-prob))
 
-       
+class branch_tree():
+    def __init__(self, features, classes, gini, parent, split_rule):
+        self.features = features
+        self.classes = classes
+        self.gini = gini
+        self.parent = parent
+        self.split_rule = split_rule
+        self.id = uuid.uuid1()
+    def set_features(self, features):    
+        self.features = features
+    def set_classes(self, classes): 
+        self.classes = classes
+    def set_gini(self, gini):
+        self.gini = gini
+    def set_parent(self, parent):    
+        self.parent = parent
+    def set_split_rule(self, split_rule):    
+        self.split_rule = split_rule
+    def dict(self):
+        d = {"features": self.features, "classes": self.classes, "gini": self.gini, "parent": self.parent, "split_rule": self.split_rule}
+        return d
+
 class composition_tree():
-    def __init__(self):
-        self.features = []
-        self.classes = []
-        self.nclasses = 0
-        self.levels = []
+    def __init__(self, nclasses=2, nfeat=4, iteration_max=100):
+        self.nclasses = nclasses
+        self.nfeat = nfeat
+        self.tree = {}
         self.queue = []
-        self.itera  tion_max = 100
-    def split(features, classes, index):
-        gini_orig = gini_impurity(classes)
+        self.iteration_max = 100
+
+    def split(self, branch):
+        features = branch.features
+        classes = branch.classes
+        parent = branch.parent
+        gini_orig = branch.gini
         split_rule = []
         classes_true = []
         classes_false = []
-        branch_true = []
-        branch_false = []
+        features_true = []
+        features_false = []
         gini_true = 0
         gini_false = 0
         N = len(classes)
         gain_gini = 0
-        for f1 in range(self.nclasses):
-            for f2 in range(self.nclasses):
-                split_true = [c for f, c in zip(features, classes) if f[index] == f1 and f[index+1] == f2]
-                split_false = [c for f, c in zip(features, classes) if not f[index] == f1 or not f[index+1] == f2]
-                g_true = gini_impurity(split_true)
-                g_false = gini_impurity(split_false)
-                g = ( g_true * (len(split_true)/N) + g_false * (len(split_false)/N) )
-                gain = gini_orig - g
-                if gain > gain_gini :
-                    gain_gini = gain
-                    gini_true = g_true
-                    gini_false = g_false
-                    split_rule = [f1, f2]     
-                    classes_true = split_true
-                    classes_false = split_false 
-                    branch_true = [f for f in features if f[index] == f1 and f[index+1] == f2]
-                    branch_false = [f for f in features if not f[index] == f1 or not f[index+1] == f2]
-        return [branch_true, branch_false], [classes_true, classes_false], [gini_true, gini_false], split_rule, gini_orig, gain_gini
+        for index in range(self.nfeat-1):
+            for f1 in range(self.nclasses):
+                for f2 in range(self.nclasses):
+                    split_true = [c for f, c in zip(features, classes) if f[index] == f1 and f[index+1] == f2]
+                    split_false = [c for f, c in zip(features, classes) if not f[index] == f1 or not f[index+1] == f2]
+                    g_true = gini_impurity(split_true)
+                    g_false = gini_impurity(split_false)
+                    g = ( g_true * (len(split_true)/N) + g_false * (len(split_false)/N) )
+                    gain = gini_orig - g
+                    if gain > gain_gini :
+                        gain_gini = gain
+                        gini_true = g_true
+                        gini_false = g_false
+                        split_rule = {"index":index, "features":[f1, f2]}     
+                        classes_true = split_true
+                        classes_false = split_false 
+                        features_true = [f for f in features if f[index] == f1 and f[index+1] == f2]
+                        features_false = [f for f in features if not f[index] == f1 or not f[index+1] == f2]
+        branch_true = branch_tree(features_true, classes_true, gini_true, branch.id, split_rule)
+        branch_false = branch_tree(features_false, classes_false, gini_false, branch.id, split_rule)
+        return branch_true, branch_false, split_rule, gain_gini
 
-    def fit(features, classes, index=1):
+    def fit(self, features, classes):
         self.features = features
         self.classes = classes
         gini = gini_impurity(classes)
-        self.tree = {"0" : [{"features": features, "classes": classes, "gini":gini } ] }
-        
-        self.queue = [ (f, c) for f, c in zip(features, classes) ]
+        root = branch_tree(features, classes, gini, 0, None)
+        root.set_parent(root.id)
+        self.tree = [ root.dict() ]
+        self.queue = [ root ]
         n = 0
-        while not len(self.queue) == 0 or n < self.iteration_max:
-            f, c = self.queue[0]:
-            branches, classes, ginis, split_rules, go, gg = split(f, c, index)
-            _ = self.queue.pop(0)
+        index = 0
+        while not len(self.queue) == 0 and n < self.iteration_max:
+            branch = self.queue.pop(0)
+            branch_true, branch_false, split_rules, gain_gini = self.split(branch)
+            print(gain_gini)
             n += 1
-            self.tree[n] = []
-            for bf, bc, bg in zip(branches, classes, ginis):
-                self.tree[n].append({"features": bf, "classes": bc, "gini":bg })
-            
-            self.tree[n].append({"features": bf, "classes": cf, "gini":gf, "branch": "False" })
-            if gg > 0:
-                
-                if gt > 0:
-                    self.queue.append((bt, ct))
-                if gf > 0:
-                    self.queue.append((bf, cf))
-        
-            
+            self.tree.append( [branch_true.dict(), branch_false.dict()] )
+            if gain_gini > 0:
+                if branch_true.gini > 0:
+                    self.queue.append(branch_true)
+                if branch_false.gini > 0:
+                    self.queue.append(branch_false)
             
         
-
-def best_split(db, lb, index):
-    gini = gini_impurity(lb)
-    split = []
-    branch_true = []
-    branch_false = []
-    N = len(lb)
-    gain_gini = 0
-    for f1 in range(nlabels):
-        for f2 in range(nlabels):
-            fit = [l for d, l in zip(db, lb) if d[index] == f1 and d[index+1] == f2]
-            notfit = [l for d, l in zip(db, lb) if not d[index] == f1 or not d[index+1] == f2]
-            g_fit = gini_impurity(fit)
-            g_notfit = gini_impurity(notfit)
-            g = ( g_fit * (len(fit)/N) + g_notfit * (len(notfit)/N) )
-            gain = gini - g
-            #print(gini, g, gain, [f1, f2])
-            if gain > gain_gini :
-                #gini = g
-                gain_gini = gain
-                split = [f1, f2]     
-                branch_true = [d for d in db if d[index] == f1 and d[index+1] == f2]
-                branch_false = [d for d in db if not d[index] == f1 or not d[index+1] == f2]
-                #print(gini, gain_gini, split)
-                
-    return branch_true, branch_false, split, gini, gain_gini
-
-
     
 ### MAIN ###
 
 
 def main(args):
-    db, lb = genfakedb()
-    c = list(zip(db, lb))
+    ft, cl = genfakedb()
+    c = list(zip(ft, cl))
     random.shuffle(c)
-    db, lb = zip(*c)
-    branch_true, branch_false, split, gini , gain = best_split(db, lb, 0)
-    print(split, gini, gain)
-    print(branch_true)
-    print(branch_false)
+    ft, cl = zip(*c)
+    compotree = composition_tree(5)
+    compotree.fit(ft, cl)
+    print(compotree.tree)
+    #branch_true, branch_false, split, gini , gain = best_split(db, lb, 0)
+    #print(branch_false)
 
 
 if __name__ == '__main__':
