d7c64665ffb7e99656ce366c00412fe7a968717e
grj <geoffrey.roman.jimenez@gmail.com>
2019-09-29 12:44:34 +0200

    modified:   CompositionTree.py

--- CompositionTree.py
+++ CompositionTree.py
@@ -72,7 +72,7 @@ def gini_impurity(data):
     prob  = np.array(prob)
     return np.sum(prob*(1-prob))
 
-class branch_tree():
+class node_tree():
     def __init__(self, features, classes, gini, parent, split_rule):
         self.features = features
         self.classes = classes
@@ -91,22 +91,22 @@ class branch_tree():
     def set_split_rule(self, split_rule):    
         self.split_rule = split_rule
     def dict(self):
-        d = {"features": self.features, "classes": self.classes, "gini": self.gini, "parent": self.parent, "split_rule": self.split_rule}
+        d = {"features": self.features, "classes": self.classes, "gini": self.gini, "id": self.id, "parent": self.parent, "split_rule": self.split_rule}
         return d
 
 class composition_tree():
-    def __init__(self, nclasses=2, nfeat=4, iteration_max=100):
+    def __init__(self, nclasses=2, nfeat=4, iteration_max=10000):
         self.nclasses = nclasses
         self.nfeat = nfeat
         self.tree = {}
         self.queue = []
-        self.iteration_max = 100
+        self.iteration_max = iteration_max
 
-    def split(self, branch):
-        features = branch.features
-        classes = branch.classes
-        parent = branch.parent
-        gini_orig = branch.gini
+    def split(self, node):
+        features = node.features
+        classes = node.classes
+        parent = node.parent
+        gini_orig = node.gini
         split_rule = []
         classes_true = []
         classes_false = []
@@ -134,34 +134,60 @@ class composition_tree():
                         classes_false = split_false 
                         features_true = [f for f in features if f[index] == f1 and f[index+1] == f2]
                         features_false = [f for f in features if not f[index] == f1 or not f[index+1] == f2]
-        branch_true = branch_tree(features_true, classes_true, gini_true, branch.id, split_rule)
-        branch_false = branch_tree(features_false, classes_false, gini_false, branch.id, split_rule)
-        return branch_true, branch_false, split_rule, gain_gini
+        node_true = node_tree(features_true, classes_true, gini_true, node.id, split_rule)
+        node_false = node_tree(features_false, classes_false, gini_false, node.id, split_rule)
+        return node_true, node_false, split_rule, gain_gini
 
     def fit(self, features, classes):
         self.features = features
         self.classes = classes
         gini = gini_impurity(classes)
-        root = branch_tree(features, classes, gini, 0, None)
+        root = node_tree(features, classes, gini, 0, None)
         root.set_parent(root.id)
-        self.tree = [ root.dict() ]
+        self.tree = [ root ]
         self.queue = [ root ]
         n = 0
         index = 0
         while not len(self.queue) == 0 and n < self.iteration_max:
-            branch = self.queue.pop(0)
-            branch_true, branch_false, split_rules, gain_gini = self.split(branch)
+            node = self.queue.pop(0)
+            node_true, node_false, split_rules, gain_gini = self.split(node)
             print(gain_gini)
             n += 1
-            self.tree.append( [branch_true.dict(), branch_false.dict()] )
+            self.tree.append( node_true ) 
+            self.tree.append( node_false ) 
             if gain_gini > 0:
-                if branch_true.gini > 0:
-                    self.queue.append(branch_true)
-                if branch_false.gini > 0:
-                    self.queue.append(branch_false)
-            
-        
-    
+                if node_true.gini > 0:
+                    self.queue.append(node_true)
+                if node_false.gini > 0:
+                    self.queue.append(node_false)
+
+    def get_root(self):
+        for node in self.tree:
+            if node.id == node.parent:     
+                return node 
+    def get_parent(self, id):
+        for node in self.tree:
+            if node.parent == id:
+                return node
+    def get_childrens(self, id):
+        childrens = []
+        for node in self.tree:
+            if node.parent == id:
+                children.append(node)
+        return childrens
+    def get_branch(self, leaf):
+        branch = [leaf]
+        node = leaf 
+        while node.parent == node.id:
+            node = self.get_parent(node.id)
+            branch.append(node)
+        return branch
+    def get_leaves(self):
+        leaves = []
+        for node in self.tree:
+            if node.gini == 0:
+                leaves.append(node)
+        return leaves 
 ### MAIN ###
 
 
@@ -172,7 +198,12 @@ def main(args):
     ft, cl = zip(*c)
     compotree = composition_tree(5)
     compotree.fit(ft, cl)
-    print(compotree.tree)
+    root = compotree.get_root().dict()
+    print("root", root)
+    leaves = compotree.get_leaves()
+    print("leaves", [l.dict() for l in leaves])
+    branches = [compotree.get_branch(l) for l in leaves]
+    print("branches", branches)
     #branch_true, branch_false, split, gini , gain = best_split(db, lb, 0)
     #print(branch_false)
 
